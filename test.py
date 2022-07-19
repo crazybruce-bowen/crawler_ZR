359935cd982bc6fe02e6b829a8b7c08804523083
Bowen Li <fake.qq.com>
2022-07-19 15:39:25 +0800

    初始化自如

---
+++ test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 19 15:32:51 2022
+
+@author: 47176
+"""
+# Environment
+
+import requests
+import pandas as pd
+import numpy as np
+from requests.exceptions import RequestException
+from pyquery import PyQuery as pq
+import json
+import time
+from multiprocessing import Pool
+import re
+
+
+#%%
+
